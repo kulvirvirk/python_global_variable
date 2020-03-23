7d74ec44d5d1e94289913beb15f5c53e3f8c6b7e
kulvirvirk <kvirkdev@gmail.com>
2020-03-23 03:05:38 +0000

    example of adding the global variable used in function

--- main.py
+++ main.py
@@ -6,6 +6,11 @@
 # 1. Define a global variable 
 global_var = 'I am global variable!'
 
+# 2. use it in the function 
+def call_global_var():
+  print('Calling from function: ' + global_var)
+
+call_global_var()
 
 
 
