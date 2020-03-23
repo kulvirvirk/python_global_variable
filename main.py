8ca824cda840a917a26c18da38efcf9d3e5bde16
kulvirvirk <kvirkdev@gmail.com>
2020-03-23 03:07:35 +0000

    added example of global variable used outside the function

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ global_var = 'I am global variable!'
 
 # 2. use it in the function 
 def call_global_var():
-  print('Calling from function: ' + global_var)
+  print('Using global varibal inside the function: ' + global_var)
 
 call_global_var()
 
+# 3. use it outside of function 
+print('Using global varibal outside the function: ' + global_var)
 
 
